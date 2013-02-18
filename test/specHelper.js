b653df44e4903d83ba670c15cf47c7cd1edcc524
CodeOfficer <codeofficer@gmail.com>
2013-02-18 00:06:36 -0800

    reset approach for ember with mocha

--- test/specHelper.js
+++ test/specHelper.js
@@ -1,1 +1,20 @@
-//
+
+Ember.testing = true;
+Ember.run.begin();
+
+before(function(){
+  Ember.run(function() {
+    App.reopen({
+      LOG_TRANSITIONS: true
+    });
+    App.Router.reopen({
+      location: 'none'
+    });
+  });
+});
+
+beforeEach(function(){
+  Ember.run(function() {
+    App.reset();
+  });
+});
