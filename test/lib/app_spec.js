dde5869f25ff7411f7a42918b2e3393c8f7b523e
CodeOfficer <codeofficer@gmail.com>
2013-03-03 16:09:44 -0500

    remove old junk

--- test/lib/app_spec.js
+++ test/lib/app_spec.js
@@ -1,26 +1,6 @@
 
 require('specHelper');
 
-describe('App', function(){
-  var container = App.__container__;
-
-  describe('reset()', function(){
-    it('creates a new container once', function(){
-      expect(container).not.to.equal(App.__container__);
-      container = App.__container__;
-    });
-
-    it('creates a new container twice', function(){
-      expect(container).not.to.equal(App.__container__);
-    });
-
-    it('keeps the app initialized', function(){
-      expect(App.get('isInitialized')).to.equal(true);
-    });
-  });
-
-});
-
 // describe('App', function(){
 //   var spy, server;
 
